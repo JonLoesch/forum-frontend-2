24ea636bbff42b6608a2f324c54ff95383ce7b46
Jonathan Loesch <jonloesch@gmail.com>
2020-05-30 13:29:43 -0400

    Moving complicated if-then-else  logic to top of component

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,36 +4,57 @@ import { like, cancelLike, dislike } from "../api";
 export function Post(props) {
   let sum = props.likes - props.dislikes;
 
+  var thumbsUpButton;
+  if (props.myVote === 1) {
+    thumbsUpButton = (
+      <i
+        className="fas fa-thumbs-up"
+        onClick={e => {
+          e.preventDefault();
+          cancelLike(props.id).then(props.refreshThread);
+        }}
+      />
+    );
+  } else {
+    thumbsUpButton = (
+      <i
+        className="far fa-thumbs-up"
+        onClick={e => {
+          e.preventDefault();
+          like(props.id).then(props.refreshThread);
+        }}
+      />
+    );
+  }
+  var thumbsDownButton;
+  if (props.myVote === -1) {
+    thumbsDownButton = (
+      <i
+        className="fas fa-thumbs-down"
+        onClick={e => {
+          e.preventDefault();
+          cancelLike(props.id).then(props.refreshThread);
+        }}
+      />
+    );
+  } else {
+    thumbsDownButton = (
+      <i
+        className="far fa-thumbs-down"
+        onClick={e => {
+          e.preventDefault();
+          dislike(props.id).then(props.refreshThread);
+        }}
+      />
+    );
+  }
+
   return (
     <div className="post">
       <span className="post-author">{props.author}</span>
       <span className="post-content">{props.content}</span>
-      <i
-        className={[props.myVote === 1 ? "fas" : "far", "fa-thumbs-up"].join(
-          " "
-        )}
-        onClick={e => {
-          e.preventDefault();
-          if (props.myVote === 1) {
-            cancelLike(props.id).then(props.refreshThread);
-          } else {
-            like(props.id).then(props.refreshThread);
-          }
-        }}
-      />
-      <i
-        className={[props.myVote === -1 ? "fas" : "far", "fa-thumbs-down"].join(
-          " "
-        )}
-        onClick={e => {
-          e.preventDefault();
-          if (props.myVote === -1) {
-            cancelLike(props.id).then(props.refreshThread);
-          } else {
-            dislike(props.id).then(props.refreshThread);
-          }
-        }}
-      />
+      {thumbsUpButton}
+      {thumbsDownButton}
       {sum}
     </div>
   );
